84783835a00b3718e5df52fa3d385502ea271b20
Bumbaclat89 <HANY89@LIVE.SE>
2025-05-02 15:35:20 +0200

    Your commit message

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
+import logo from "../assets/logo.png"; // adjust path as needed
 
 function Logo() {
   return (
     <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      <img src={logo} alt="WorldWise logo" className={styles.logo} />
     </Link>
   );
 }
